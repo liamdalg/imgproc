5d3f695cea1ace0d351c893c047fd2d3d8f566e0
liamdalg <liamdalg99@gmail.com>
2018-10-03 23:29:50 +0100

    initial commit

---
+++ imgproc.py
@@ -0,0 +1,19 @@
+"""Image processing module.
+
+Provides functionality for processing images,
+such as edge detection, filters, etc.
+"""
+
+from PIL import Image
+
+
+__version__ = '0.1'
+__author__ = 'liamdalg'
+
+
+def sobel_operator(img):
+    return img
+
+
+def laplacian_operator(img):
+    return img
