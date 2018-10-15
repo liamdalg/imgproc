93921b6d18e679033f7c7c564a60deaf1467067e
liamdalg <liamdalg99@gmail.com>
2018-10-15 17:07:16 +0100

    basic 2d kernel convolution

--- imgproc.py
+++ imgproc.py
@@ -10,7 +10,17 @@ import cv2 as cv
 __version__ = '0.1'
 __author__ = 'liamdalg'
 
-def sobel_operator(img):
+def _kernel_convolution_2d(arr: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    rows, cols = kernel.shape
+    x, y = rows // 2, cols // 2
+
+    conv = np.empty(arr.shape)
+    padded_arr = np.pad(np.copy(arr), ((x, y), (x, y)), mode='constant')
+    for i in range(arr.shape[0]):
+        for j in range(arr.shape[1]):
+            conv[i][j] = np.sum(padded_arr[i:(i + rows), j:(j + cols)] * kernel)
+
+    return conv
     return img
 
 
