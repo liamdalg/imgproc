6b6f3a7588b5c380b2ac4165ddae793cc2d030bf
liamdalg <liamdalg99@gmail.com>
2018-10-16 13:08:56 +0100

    draft sobel function with threshold

--- imgproc.py
+++ imgproc.py
@@ -22,13 +22,21 @@ def _kernel_convolution_2d(arr: np.ndarray, kernel: np.ndarray) -> np.ndarray:
 
     return conv
 
-def sobel_operator(img: np.ndarray) -> np.ndarray:
+def sobel_operator(img: np.ndarray, threshold: int) -> np.ndarray:
     sobel_x = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
     x = _kernel_convolution_2d(img, sobel_x)
     y = _kernel_convolution_2d(img, sobel_y)
     cv.imwrite('outx.jpg', x)
     cv.imwrite('outy.jpg', y)
+    
+    combined = np.empty(x.shape)
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            val = int(((x[i,j] ** 2) + (y[i,j] ** 2)) ** 0.5)
+            combined[i,j] = val if val > threshold else 0
+    
+    cv.imwrite('outcombined.jpg', combined)
     return img
 
 
