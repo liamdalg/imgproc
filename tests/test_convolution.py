2f95b9efc32cb23b8925cac5fd5c35af59d17971
liamdalg <liamdalg99@gmail.com>
2018-10-16 13:51:17 +0100

    add a 4x3 convolution test

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -2,8 +2,11 @@ import numpy as np
 import unittest
 import imgproc
 
+def format_expected(expected, actual):
+    return 'Expected Value: {}\nActual Value:{}'.format(expected,actual)
+
 class TestConvolutions(unittest.TestCase):
-    """Tests method related to kernel convolutions.
+    """Tests methods related to kernel convolutions.
     """
 
     def test_basic_3x3_conv(self):
@@ -12,7 +15,16 @@ class TestConvolutions(unittest.TestCase):
         true_conv = np.array([[-13, -20, -17], [-18, -24, -18], [13, 20, 17]])
         conv = imgproc._kernel_convolution_2d(source, kernel)
 
-        self.assertTrue(np.array_equiv(true_conv, conv), 'Expected Value:\n{}\nActual Value:\n{}'.format(true_conv, conv))
+        self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
+
+    def test_basic_4x3_conv(self):
+        source = np.array([[1, 5, 2, 3], [8, 7, 3, 6], [3, 3, 9, 1]])
+        kernel = np.array([[1, 2, 3], [0, 0, 0], [6, 5, 4]])
+        true_conv = np.array([[23, 41, 33, 21], [44, 65, 76, 52], [82, 85, 79, 42]])
+        conv = imgproc._kernel_convolution_2d(source, kernel)
+
+        self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
+
 
 if __name__ == '__main__':
     unittest.main()
