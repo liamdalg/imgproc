85bcb84b1502329d314a02332c52714bcf2bd718
liamdalg <liamdalg99@gmail.com>
2018-10-16 13:33:15 +0100

    fixed kernel not being flipped before convolution

--- imgproc.py
+++ imgproc.py
@@ -13,12 +13,13 @@ __author__ = 'liamdalg'
 def _kernel_convolution_2d(arr: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     rows, cols = kernel.shape
     x, y = rows // 2, cols // 2
+    flipped_kernel = np.fliplr(np.flipud(kernel))
 
     conv = np.empty(arr.shape)
     padded_arr = np.pad(np.copy(arr), ((x, y), (x, y)), mode='constant')
     for i in range(arr.shape[0]):
         for j in range(arr.shape[1]):
-            conv[i][j] = np.sum(padded_arr[i:(i + rows), j:(j + cols)] * kernel)
+            conv[i][j] = np.sum(padded_arr[i:(i + rows), j:(j + cols)] * flipped_kernel)
 
     return conv
 
