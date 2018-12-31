e68f76c2dcd50e0ed1443245290c44db938cc66b
liamdalg <liamdalg99@gmail.com>
2018-12-31 00:47:29 +0000

    fix tests

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -1,6 +1,6 @@
 import numpy as np
 import unittest
-import imgproc
+import imgproc_cy
 
 def format_expected(expected, actual):
     return 'Expected Value: {}\nActual Value:{}'.format(expected,actual)
@@ -10,18 +10,18 @@ class TestConvolutions(unittest.TestCase):
     """
 
     def test_basic_3x3_conv(self):
-        source = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-        true_conv = np.array([[-13, -20, -17], [-18, -24, -18], [13, 20, 17]])
-        conv = imgproc._kernel_convolution_2d(source, kernel)
+        source = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).astype(np.double)
+        kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]).astype(np.double)
+        true_conv = np.array([[-13, -20, -17], [-18, -24, -18], [13, 20, 17]]).astype(np.double)
+        conv = np.asarray(imgproc_cy._kernel_convolution_2d(source, kernel))
 
         self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
 
     def test_basic_4x3_conv(self):
-        source = np.array([[1, 5, 2, 3], [8, 7, 3, 6], [3, 3, 9, 1]])
-        kernel = np.array([[1, 2, 3], [0, 0, 0], [6, 5, 4]])
-        true_conv = np.array([[23, 41, 33, 21], [44, 65, 76, 52], [82, 85, 79, 42]])
-        conv = imgproc._kernel_convolution_2d(source, kernel)
+        source = np.array([[1, 5, 2, 3], [8, 7, 3, 6], [3, 3, 9, 1]]).astype(np.double)
+        kernel = np.array([[1, 2, 3], [0, 0, 0], [6, 5, 4]]).astype(np.double)
+        true_conv = np.array([[23, 41, 33, 21], [44, 65, 76, 52], [82, 85, 79, 42]]).astype(np.double)
+        conv = np.asarray(imgproc_cy._kernel_convolution_2d(source, kernel))
 
         self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
 
