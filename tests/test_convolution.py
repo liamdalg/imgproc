f2fd953d93a1d7196a9f2d76a6adf824f81e8f55
liamdalg <liamdalg99@gmail.com>
2019-01-06 00:27:16 +0000

    fix tests

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -1,6 +1,6 @@
 import numpy as np
 import unittest
-import imgproc_cy
+import convolution
 
 def format_expected(expected, actual):
     return 'Expected Value: {}\nActual Value: {}'.format(expected,actual)
@@ -13,7 +13,7 @@ class TestConvolutions(unittest.TestCase):
         source = np.array([[1]]).astype(np.float64)
         kernel = np.array([[1, 1, 1], [1, 2, 1], [1, 1, 1]]).astype(np.float64)
         true_conv = np.array([[2]]).astype(np.float64)
-        conv = np.asarray(imgproc_cy._kernel_convolution_2d(source, kernel))
+        conv = np.asarray(convolution.kernel_convolution_2d(source, kernel))
 
         self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
 
@@ -21,7 +21,7 @@ class TestConvolutions(unittest.TestCase):
         source = np.array([[1], [2], [3]]).astype(np.float64)
         kernel = np.array([[1, 5, 1], [1, 5, 1], [1, 5, 1]]).astype(np.float64)
         true_conv = np.array([[15], [30], [25]]).astype(np.float64)
-        conv = np.asarray(imgproc_cy._kernel_convolution_2d(source, kernel))
+        conv = np.asarray(convolution.kernel_convolution_2d(source, kernel))
 
         self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
 
@@ -29,7 +29,7 @@ class TestConvolutions(unittest.TestCase):
         source = np.array([[1, 2, 3]]).astype(np.float64)
         kernel = np.array([[1, 1, 1], [10, 10, 10], [1, 10, 1]]).astype(np.float64)
         true_conv = np.array([[30, 60, 50]]).astype(np.float64)
-        conv = np.asarray(imgproc_cy._kernel_convolution_2d(source, kernel))
+        conv = np.asarray(convolution.kernel_convolution_2d(source, kernel))
 
         self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
 
@@ -37,7 +37,7 @@ class TestConvolutions(unittest.TestCase):
         source = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).astype(np.float64)
         kernel = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]).astype(np.float64)
         true_conv = np.array([[-13, -20, -17], [-18, -24, -18], [13, 20, 17]]).astype(np.float64)
-        conv = np.asarray(imgproc_cy._kernel_convolution_2d(source, kernel))
+        conv = np.asarray(convolution.kernel_convolution_2d(source, kernel))
 
         self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
 
@@ -45,7 +45,7 @@ class TestConvolutions(unittest.TestCase):
         source = np.array([[1, 5, 2, 3], [8, 7, 3, 6], [3, 3, 9, 1]]).astype(np.float64)
         kernel = np.array([[1, 2, 3], [0, 0, 0], [6, 5, 4]]).astype(np.float64)
         true_conv = np.array([[23, 41, 33, 21], [44, 65, 76, 52], [82, 85, 79, 42]]).astype(np.float64)
-        conv = np.asarray(imgproc_cy._kernel_convolution_2d(source, kernel))
+        conv = np.asarray(convolution.kernel_convolution_2d(source, kernel))
 
         self.assertTrue(np.array_equal(true_conv, conv), format_expected(true_conv, conv))
 
